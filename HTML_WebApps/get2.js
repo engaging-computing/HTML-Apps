70e20fa6c2b5017d31c0faed570bf8e14fefa00d
JasonD94 <jason_downing@student.uml.edu>
2014-06-18 15:42:24 -0400

    Cleaned up UI

--- HTML_WebApps/get2.js
+++ HTML_WebApps/get2.js
@@ -8,14 +8,23 @@ function submitter()
 							async: false,
 							dataType: "JSON"
 							}).responseText;
-
-	// Now we should have some information about the project.
-	var object = arg[0].name;
-	rev.innerHTML = "The closest project I have to that name is: <br/>"+ object;
-	console.log(arg[0]);
+	console.log(response);
 	
-	// DEBUG
-	//rev.innerHTML = response;
-	//console.log(response);
-
+	if(response === undefined)
+	{
+		rev.innerHTML = "That project ID could not be found!";
+	}
+	else{
+	
+	// Now we should have some information about the project.
+	var arg = JSON.parse(response);
+	var array = [];
+	array[0] = arg['name'];
+	array[1] = arg['createdAt'];
+	array[2] = arg['ownerName'];
+	array[3] = arg['url'];
+	
+	rev.innerHTML = "Information about this project: <br/>Name: "+ array[0] + "<br/>Created at: "+ array[1] + "<br/> Owner: "+ array[2] + "<br/>URL: " + array[3];
+	console.log(arg);
+	}
 }
