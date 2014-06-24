d14d349d05b290086a55c8e5e5441c144d2c0859
JasonD94 <jason_downing@student.uml.edu>
2014-06-24 14:26:55 -0400

    Eye color X & Y done.

--- USASEF/eye.js
+++ USASEF/eye.js
@@ -1,6 +1,24 @@
-function clicked()
+document.addEventListener("DOMContentLoaded", init, false);
+
+function init()
 {
+	var canvas = document.getElementById("world");
+	canvas.addEventListener("mousedown", getPosition, false);
+}
+
+function getPosition(event)
+{
+	var canvas = document.getElementById("world");
+
+	var x = event.x - canvas.offsetLeft;
+	var y = event.y - canvas.offsetTop;
+
+	console.log("x: " + x + "  y: " + y);
 	
-
-
+	coord_x.innerHTML = x;
+	coord_y.innerHTML = y;
+	
+	// Figure out how to convert coordinates to GPS?
+	GPS_X.innerHTML = "xx";
+	GPS_Y.innerHTML = "xx";
 }
