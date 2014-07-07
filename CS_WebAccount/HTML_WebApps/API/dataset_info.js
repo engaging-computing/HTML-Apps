319a99856ae5da4930b8e45c109e63f2bbe58393
Jason <jason_downing@student.uml.edu>
2014-07-07 14:22:44 -0400

    Added link in Dataset Info app.

--- CS_WebAccount/HTML_WebApps/API/dataset_info.js
+++ CS_WebAccount/HTML_WebApps/API/dataset_info.js
@@ -2,7 +2,8 @@ function submitter()
 {
 	var ID = document.getElementById("dataset_name").value;
 	var URL = "http://isenseproject.org/api/v1/projects/"+ID;
-
+	var USER_URL_TEXT = "Click here to view project #" + ID + "!";
+	
 	var response = $.ajax({ type: "GET",
 							url: URL,
 							async: false,
@@ -24,7 +25,9 @@ function submitter()
 	array[2] = arg['ownerName'];
 	array[3] = arg['url'];
 	
-	rev.innerHTML = "Information about this project: <br/>Name: "+ array[0] + "<br/>Created at: "+ array[1] + "<br/> Owner: "+ array[2] + "<br/>URL: " + array[3];
-	console.log(arg);
+	rev.innerHTML = "Information about this project: <br/>Name: "+ array[0] 
+	+ "<br/>Created at: "+ array[1] + "<br/> Owner: "+ array[2] 
+	+ '<br/>URL: <a href="' + array[3] + '">' + USER_URL_TEXT + '</a>';
+
 	}
 }
