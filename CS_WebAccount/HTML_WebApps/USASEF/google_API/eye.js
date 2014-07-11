7658bf80df1db543507d35c74e02cade1677b4cc
Jason <jason_downing@student.uml.edu>
2014-07-11 12:57:16 -0400

    Started Google API work

--- CS_WebAccount/HTML_WebApps/USASEF/google_API/eye.js
+++ CS_WebAccount/HTML_WebApps/USASEF/google_API/eye.js
@@ -9,34 +9,64 @@ var API_URL;
 var USER_URL;
 var USER_URL_TEXT = 'Click here to go to your project!';
 
-function zoom_in() {
-	zoom_num = zoom_num + .15;
-	
-	$(document).ready(function() {
-		$('body').css('zoom', zoom_num); /* Webkit browsers */
-		//$('body').css('zoom', zoom_num); /* Other non-webkit browsers */
-		//$('body').css('-moz-transform', scale(zoom_num, zoom_num)); /* Moz-browsers */
+// Google Maps API variables
+var map = null;
+var marker = null;
+
+var infowindow = new google.maps.InfoWindow(
+{ 
+	size: new google.maps.Size(150,50)
+});
+
+
+// A function to create the marker and set up the event window function 
+function createMarker(latlng, name, html) {
+    var contentString = html;
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        zIndex: Math.round(latlng.lat()*-100000)<<5
 	});
+
+    google.maps.event.addListener(marker, 'click', 
+    function() {
+        infowindow.setContent(contentString); 
+        infowindow.open(map,marker);
+	});
+        
+    google.maps.event.trigger(marker, 'click');    
+    return marker;
 }
 
-function zoom_out() {
-	zoom_num = zoom_num - .15;
+// Initialize the map.
+function initialize() {
+	var myOptions = {
+		zoom: 2,
+		center: new google.maps.LatLng(0, 0),
+		mapTypeControl: true,
+		mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+		navigationControl: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
 	
-	$(document).ready(function() {
-		$('body').css('zoom', zoom_num); /* Webkit browsers */
-		//$('body').css('zoom', zoom_num); /* Other non-webkit browsers */
-		//$('body').css('-moz-transform', scale(zoom_num, zoom_num)); /* Moz-browsers */
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+ 
+	google.maps.event.addListener(map, 'click', 
+	function() {
+    	infowindow.close();
 	});
-}
 
-function zoom_reset() {
-	zoom_num = 1.0;
+	google.maps.event.addListener(map, 'click', 
+	function(event) {
 	
-	$(document).ready(function() {
-		$('body').css('zoom', zoom_num); /* Webkit browsers */
-		//$('body').css('zoom', zoom_num); /* Other non-webkit browsers */
-		//$('body').css('-moz-transform', scale(zoom_num, zoom_num)); /* Moz-browsers */
+		//call function to create marker
+		if (marker) {
+			marker.setMap(null);
+			marker = null;
+		}
+		marker = createMarker(event.latLng, "name", "<b>Location</b><br>"+event.latLng);
 	});
+
 }
 
 function popup_user() {
