05f6ec040b26c7c7f3231b4298a4cb5dfb8ac8d5
Tyler Puleo <tyler.puleo22@gmail.com>
2014-07-15 11:36:16 -0400

    GPS App first commit

---
+++ Phonegap/Apps/gps/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/tylerpuleo/gps/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/tylerpuleo/gps/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ Phonegap/Apps/gps/platforms/android/ant-gen/R.java.d
@@ -0,0 +1,14 @@
+/home/tylerpuleo/gps/platforms/android/ant-gen/com/phonegap/helloworld/R.java \
+ : /home/tylerpuleo/gps/platforms/android/res/drawable-mdpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/res/drawable/icon.png \
+/home/tylerpuleo/gps/platforms/android/res/drawable-xhdpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/res/drawable-hdpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/res/drawable-ldpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/res/xml/config.xml \
+/home/tylerpuleo/gps/platforms/android/res/values/strings.xml \
+/home/tylerpuleo/gps/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/ant-build/res/drawable/icon.png \
+/home/tylerpuleo/gps/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/home/tylerpuleo/gps/platforms/android/ant-build/AndroidManifest.xml \
