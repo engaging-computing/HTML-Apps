af46aca0307c6a52293f00efa7b6958f1fb77a93
Jason <jason_downing@student.uml.edu>
2014-07-17 15:45:42 -0400

    CSS Styling somewhat complete

--- HTML-5/Apps/CS_WebAccount/HTML_WebApps/Plinko_Probability/plinko.js
+++ HTML-5/Apps/CS_WebAccount/HTML_WebApps/Plinko_Probability/plinko.js
@@ -56,9 +56,9 @@ function submitter()
 		$.post(API_URL, upload);
 		
 		// Add a link in the HTML file to the project they contributed to.
-		The_URL.innerHTML = '<br/><a href="'+ USER_URL +'">' + USER_URL_TEXT + '</a> <br/> <br/>';
+		The_URL.innerHTML = '<a href="'+ USER_URL +'">' + USER_URL_TEXT + '</a>';
 	}
 	else {
-		The_URL.innerHTML = "<br/>Canceled!<br/><br/>";
+		The_URL.innerHTML = "Canceled!";
 	}
 }
