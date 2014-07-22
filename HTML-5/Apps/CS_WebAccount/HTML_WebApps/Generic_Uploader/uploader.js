a0ac2d04954c43427945eb47a6de89e216e96329
Jason <jason_downing@student.uml.edu>
2014-07-22 13:26:53 -0400

    More work on an HTML5 Web uploader to iSENSE. So far - able to get fields from a project.

--- HTML-5/Apps/CS_WebAccount/HTML_WebApps/Generic_Uploader/uploader.js
+++ HTML-5/Apps/CS_WebAccount/HTML_WebApps/Generic_Uploader/uploader.js
@@ -1,27 +1,101 @@
+var email = null;
+var password = null;
+var contributor_key = null;
+var PROJECT_ID = null;
+
+var API_URL = null;
+var USER_URL = null;
+var USER_URL_TEXT = null;
+
 function get_fields() {
+	// Get information that the user entered.
+	          email = document.getElementById("email").value;
+	       password = document.getElementById("password").value;
+	contributor_key = document.getElementById("contributor_key").value;
+	     PROJECT_ID = document.getElementById("project_ID").value;
+
+	console.log("Proj ID = " + PROJECT_ID);
+	
+	/* 	Now pull the fields off iSENSE.
+		We will need to do a GET request.
+		Use the "Projects" address - /api/v1/projects/XX
+		which XX will be replaced by the project_ID variable we got.
+	*/
+	
+	// Set the URLs needed for later.
+	      API_URL = 'http://rsense-dev.cs.uml.edu/api/v1/projects/' + PROJECT_ID;
+	     USER_URL = 'http://rsense-dev.cs.uml.edu/projects/' + PROJECT_ID;
+	USER_URL_TEXT = 'Click here to go to your project!';
+	
+	console.log("API_URL = " + API_URL);
+	
+	// Make the GET request
+	var response = $.ajax({ type: "GET",
+							 url: API_URL,
+						   async: false,
+					    dataType: "JSON"
+	}).responseText;
+	
+	console.log(response);
+	
+	if(response === undefined)
+	{
+		resp.innerHTML = "That Project ID could not be found!";
+	}
+	else{
+		resp.innerHTML = "Hey, we got something usable! Write more code!";
 		
-	// This will be figured out another day.
-	
-	
+		// Let's try printing all the fields to the HTML doc!
+
+		var arg = JSON.parse(response);
+		console.log(arg);
+		
+		var js_Obj = arg["fields"];
+		var fields = [];
+		var i = 0;
+		
+		// Console.log all the fields.
+		for(var key in js_Obj) {
+			fields[i] = js_Obj[key];
+			console.log(fields[i]);
+			i++;
+		}
+		
+		// Array now
+	}
+
 }
 // Some of this is probably needed.
 
-//// Global variables for keeping track of what the user has selected.
-//var start_choice = 0, end_choice = 0;
 
-//// Set the start variable when the user clicks a button.
-//function start(choice) {
-//	start_choice = choice;
-//	START.innerHTML = start_choice;
-//	//console.log(start_choice); 	// DEBUGGING.
-//}
+	/* STUFF FROM THE FIELD SEARCHER API TEST CAN BE USED HERE */
+	/*
+	
+	console.log(response);
+	
+	if(response === undefined)
+	{
+		rev.innerHTML = "That field ID could not be found!";
+	}
+	else{
+	
+	// Now we should have some information about the project.
+	var arg = JSON.parse(response);
+	var array = [];
+	array[0] = arg['name'];
+	array[1] = arg['type'];
+	array[2] = arg['unit'];
+	array[3] = arg['restrictions'];
+	
+	rev.innerHTML = "Information about this field: <br/>Name: "+ array[0] +
+	 "<br/>Type: "+ array[1] + "<br/> Unit: "+ array[2] + 
+	 "<br/>Restrictions: " + array[3];
+	console.log(arg);
+	}
+	
+	*/
 
-//// Ending variable gets set here.
-//function end(choice) {
-//	end_choice = choice;
-//	END.innerHTML = end_choice;
-//	//console.log(end_choice);		// DEBUGGING
-//}
+
 
 //function submitter() 
 //{
@@ -32,9 +106,7 @@ function get_fields() {
 //	}
 //	
 //	// Make the URL links.
-//	var API_URL = 'http://isenseproject.org/api/v1/projects/598/jsonDataUpload';
-//	var USER_URL = 'http://isenseproject.org/projects/598';
-//	var USER_URL_TEXT = 'Click here to go to your project!';
+
 
 //	/* 	Get current time - used for making the title different 
 //		every time the user uploads data. 	*/
