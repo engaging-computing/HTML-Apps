88d2d13d75936a2c85f882445742bb7742d28d25
Jason <jason_downing@student.uml.edu>
2014-08-12 13:27:01 -0400

    added comments to the boat house js file

--- HTML-5/Apps/BoatHouse_App/boathouse.js
+++ HTML-5/Apps/BoatHouse_App/boathouse.js
@@ -1,59 +1,118 @@
 function submitter()
 {
-	// Make the URL links.
+	/*
+		Here are all the various URLs that are used.
+		API_URL => used in making the POST to iSENSE. 
+		(Check out the iSENSE API for more information.)
+		
+		USER_URL & USER_URL_TEXT => these are for the dynamic links.
+		(Note the differences between the API_URL and the USER_URL!
+		 /api/v1/projects/PROJECT ID/jsonDataUpload is for the API
+		 /projects/PROJECT ID is for linking someone or yourself to the iSENSE
+		 																website.)
+	*/
 	var API_URL = 'http://isenseproject.org/api/v1/projects/556/jsonDataUpload';
 	var USER_URL = 'http://isenseproject.org/projects/556';
 	var USER_URL_TEXT = 'Click here to go to your project!';
-
-	// Get current time - used for timestamp
+	
+	/*
+		Change this portion with your own email & password.
+		You can also customize the app so the user can put their own 
+		email and password.
+	*/
+	var    email = 'j@j.j';
+	var password = 'j';
+	
+	/*
+		This portion gets a timestamp by using the JavaScript new Data() function.
+		Stringify the timestamp to get the correct format for iSENSE.
+	*/
 	var currentTime = new Date();
 	var timestamp = JSON.stringify(currentTime);
 
-	// Get the variables that the user entered in the HTML portion of the app.
-	var formData = [];
-
-	formData.push(document.boathouse.tp.value);
-	formData.push(document.boathouse.ph.value);
-	formData.push(document.boathouse.o2.value);
-	formData.push(document.boathouse.phos.value);
-
-	// get the letter they entered. Make it easier to submit the other data.
+	/*
+		This portion grabs all the data that the user entered in the HTML.
+		This is done using the document.getElementById function of JavaScript,
+		which given an element (a button, selection, form, etc) and its ID, it
+		can get the value of that element. It can also be used to change the element,
+		or append stuff to the HTML document.
+	*/
 	var letter = document.getElementById("table_val").value;
-	var temp = formData[0];
-	var ph = formData[1];
-	var dox = formData[2];
-	var phos = formData[3];
+	var temp = document.getElementById("tp").value;
+	var ph = document.getElementById("ph").value;
+	var dox = document.getElementById("o2").value;
+	var phos = document.getElementById("phos").value;
 
-	/*  In the future: allow different projects, contributor keys and username
-		and passwords. 	*/
-
-	// Data to be uploaded to iSENSE
+	/*
+		This is the main data that will be uploaded to iSENSE.
+		The right side must be in quotes, and set up as it is in order to do
+		a post to the iSENSE dataset jsonDataUpload API. The iSENSE API 
+		documentation can be found at http://isenseproject.org/api/v1/docs
+	*/
 	var upload = {
-		'email': 'j@j.j',
-		'password': 'j',
-		'title': [],
-		'data':
-	  	{
-	  		'2685': [timestamp],
-			// LAT AND LONG ARE NOT USED FOR THIS WEB APP. Although it could be!
-			'2640': [temp],
-			'2641': [ph],
-			'2642': [dox],
-			'2643': [phos]
+		'email': [email],			// Email, Password, Title and data are all
+		'password': [password],		// Required. Email/Password/Title must be strings.
+		'title': [],				// To use variables instead of text, put the variables
+		'data':						// inside brackets.
+	  	{							// If you want to hardcode the email or any other
+	  								// variable, put the text in quotes like so:
+	  		'2685': [timestamp],	// 'generic_email@gmail.com'
+			'2640': [temp],			// the full line would look like this:
+			'2641': [ph],			// 'email': 'generic_email@gmail.com'
+			'2642': [dox],			// You could also just set the email variable
+			'2643': [phos]			// to generic_email@gmail.com.
+
+			/*
+				One other note about fields - they are UNIQUE to each project ID.
+				If you decide to make your own project, you WILL need to change these
+				fields - that is, the field IDs, which are the numbers in quotes on the
+				left hand side. To find the field IDs for your project, you can go
+				to the folllowing address, making sure to replace "--PROJECT ID--"
+				with your own project ID:
+				http://isenseproject.org/api/v1/projects/--PROJECT ID--/
+				
+				The field IDs are listed at the end. An example of one field ID from
+				project 556 is this:
+				
+				{"id":2685,"name":"Timestamp","type":1,"unit":null,"restrictions":null}
+			*/
 	 	}
 	}
 
-	// Modify the title to be either A, B or C
+	/*
+		This portion modifies the title to include the letter of the table,
+		and a timestamp in the title to 'randomize' the title. This is because
+		iSENSE datasets cannot have the exact same title. If you attempt to hard
+		code in a title, such as "table" and then try to upload a dataset several
+		times with "table" as the title, you WILL get an error from the iSENSE API.
+		Most likely: 
+	*/
 	upload.title = "Table " + [letter] + " " + [timestamp];
 
+	/*
+		This part does the actual uploading to iSENSE.
+		Note, the $.post() function is a jquery library. Make sure to include
+		one in your HTML file. Google has one that can easily be included - see
+		the HTML doc for more information.
+		
+		Also, the if/else isn't necessarily required by iSENSE. I used it to detect
+		if the user wants to quit/disregard the current upload.
+	*/
+	// Side note - the confirm part is a JavaScript popup that just asks for "OK" or "Cancel".
 	if(confirm("Do you want to upload this data to iSENSE?")) {
-		// Post to iSENSE
+		
+		/* 
+			The POST command. All that is required for iSENSE datasets is the API_URL,
+			and the data, which should be formatted as outlined in the API docs.
+			See the above upload variable for how to do so.
+		*/
 		$.post(API_URL, upload);
 
 		// Add a link in the HTML file to the project they contributed to.
 		The_URL.innerHTML = '<p><a href="'+ USER_URL +'">' + USER_URL_TEXT + '</a></p>';
 	}
 	else {
+		// Change the HTML to show that the user canceled the upload request.
 		The_URL.innerHTML = "<br/>Canceled!<br/><br/>";
 	}
 }
