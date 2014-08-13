2204e252e1758104451e6868542ebb59b689e105
Jason <jason_downing@student.uml.edu>
2014-08-13 12:41:10 -0400

    GET project info by Project ID is now commented and added to the js library

--- HTML5_Library/isense_libary.js
+++ HTML5_Library/isense_libary.js
@@ -17,7 +17,9 @@ function POST_jsonDataUpload_Email()
 		'title': title,				 
 		'data':						
 	  	{							 			 
-	  		// Fill in the fields for your project here.		 
+	  		// 	Fill in the fields for your project here.
+	  		// 	use the following URL to find this:
+	  		//	http://isenseproject.org/api/v1/projects/PROJECT_ID/		 
 	 	}
 	}
 	
@@ -40,7 +42,6 @@ function POST_jsonDataUpload_ContributorKey()
 	var USER_URL;		// EX: 'http://isenseproject.org/projects/555';
 	var USER_URL_TEXT	// EX: 'Click here to go to your project!';
 
-
 	var currentTime = new Date();
 	var timestamp = JSON.stringify(currentTime);
 
@@ -59,7 +60,9 @@ function POST_jsonDataUpload_ContributorKey()
 		'contributor_name': name,
 		'data':
 	  	{
-			'2639': [formData]
+			// 	Fill in the fields for your project here.
+			// 	Use the following URL to find this:
+	  		//	http://isenseproject.org/api/v1/projects/PROJECT_ID/
 	 	}
 	}
 
@@ -81,5 +84,52 @@ function POST_jsonDataUpload_ContributorKey()
 	else {
 		// Change the HTML to show that the user canceled the upload request.
 		The_URL.innerHTML = "<p>Canceled!</p>";
+	}
+}
+
+function GET_Projects_ByProjectID()
+{
+	var ID; 				// EX: = document.getElementById("dataset_name").value;
+	var URL;				// EX: "http://isenseproject.org/api/v1/projects/"+ID;
+	var USER_URL_TEXT;		// EX: "Click here to view project #" + ID + "!";
+	
+	/*	type: 	Must be either "POST" or "GET" for iSENSE - in this case it MUST be
+				a "GET" request because we aren't posting anything to iSENSE.
+		url: use the URL variable at the top. You could also hardcode this in.
+		async: false, because we want to make a cross domain request.
+		dataType: "JSON" because we are requesting the data in JSON format.		*/
+	var response = $.ajax({ type: "GET",
+							url: URL,
+							async: false,
+							dataType: "JSON"
+							}).responseText;
+
+	/*	If the response text ends up being undefined, we weren't able to find
+		anything about the project.			*/ 
+	if(response === undefined)
+	{
+		rev.innerHTML = "That project ID could not be found!";
+	}
+	else{
+
+		/* 	Now we should have some information about the project. This should
+			let us parse the request and then store the information about the
+			project.			*/
+		var arg = JSON.parse(response);
+		var array = [];
+		array[0] = arg['name'];
+		array[1] = arg['createdAt'];
+		array[2] = arg['ownerName'];
+		array[3] = arg['url'];
+
+		/*	Here I display some of the information about the project.
+			
+			Note: 	Use the output of the console.log(response) command to find
+					out more information about a given project. There are other
+					properties you can display to the user.		*/
+		rev.innerHTML = "Information about this project: <br/>Name: "+ array[0]
+		+ "<br/>Created at: "+ array[1] + "<br/> Owner: "+ array[2]
+		+ '<br/>URL: <a href="' + array[3] + '">' + USER_URL_TEXT + '</a>';
+
 	}
 }
