6480ff6dd514ca5ce2661577532302d3e5b3bbd7
Jason <jason_downing@student.uml.edu>
2014-08-13 11:50:31 -0400

    Added contributor key function to isense js library

--- HTML5_Library/isense_libary.js
+++ HTML5_Library/isense_libary.js
@@ -33,3 +33,53 @@ function POST_jsonDataUpload_Email()
 		The_URL.innerHTML = "<br/>Canceled!<br/><br/>";
 	}
 }
+
+function POST_jsonDataUpload_ContributorKey()
+{
+	var API_URL;		// EX: 'http://isenseproject.org/api/v1/projects/555/jsonDataUpload';
+	var USER_URL;		// EX: 'http://isenseproject.org/projects/555';
+	var USER_URL_TEXT	// EX: 'Click here to go to your project!';
+
+
+	var currentTime = new Date();
+	var timestamp = JSON.stringify(currentTime);
+
+	var   key;		// EX: '12345';
+	var  name;		// EX: 'WebApp';
+	var title;		// EX: 'My awesome WebApp!'
+	
+	/* 	You probably want to use some JavaScript functions here, such as 
+		document.getElementById("PUT ID HERE").value;
+		This way you can get user input and then submit that data to iSENSE.
+	*/
+
+	var upload = {
+		'title': title,
+		'contribution_key': key,
+		'contributor_name': name,
+		'data':
+	  	{
+			'2639': [formData]
+	 	}
+	}
+
+	// POST the data to iSENSE
+	if(confirm("Do you want to upload this data to iSENSE?")) {
+		var result = $.post(API_URL, upload);
+
+		/*	This is quick check to see if the above POST command worked. */
+		if(result.done) {
+			rev.innerHTML = "<p>Successfully posted to iSENSE!</p>";
+		}
+		else if(result.fail) {
+			rev.innerHTML = "<p>Failed to post to iSENSE!</p>";
+		}
+
+		// Add a link in the HTML file to the project they contributed to.
+		The_URL.innerHTML = '<p><a href="'+ USER_URL +'">' + USER_URL_TEXT + '</a></p>';
+	}
+	else {
+		// Change the HTML to show that the user canceled the upload request.
+		The_URL.innerHTML = "<p>Canceled!</p>";
+	}
+}
